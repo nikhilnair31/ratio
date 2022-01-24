a26102cf7b3b76fa83cc3379566f9611b52a21b7
Nikhil Nair <niknair3189@gmail.com>
2022-01-24 23:25:51 +0530

    Added some share stuff in Search Page

--- screens/SearchPage.js
+++ screens/SearchPage.js
@@ -1,11 +1,25 @@
-import React, {useLayoutEffect} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { TouchableOpacity, StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import React, {useEffect, useLayoutEffect, useRef, useState} from 'react';
+import { Share, TouchableOpacity, StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { Avatar, Button, Text, Input, Image } from 'react-native-elements';
+import { captureRef } from 'react-native-view-shot';
+import * as Linking from 'expo-linking';
 
 const SearchPage = ({navigation}) => {
+    const viewRef = useRef();
+
     const goToProfileTab = () => {
         navigation.replace('ProfilePage')
+    }
+    const test = async () => { 
+        Linking.openURL(`instagram://user?username=apple`)
+        // const uri = await captureRef(viewRef, {
+        //     format: 'png',
+        //     quality: 0.5,
+        // });
+        // const encodedURL = encodeURIComponent(uri)
+        // let instagramURL = 'instagram://library?AssetPath='+encodedURL; 
+        // console.log('instagramURL: ', instagramURL); 
+        // Linking.openURL(instagramURL); 
     }
 
     useLayoutEffect(() => {
@@ -20,8 +34,9 @@ const SearchPage = ({navigation}) => {
     }, [navigation]);
     
     return (
-        <KeyboardAvoidingView behaviour='padding' enabled style={styles.container}>
+        <KeyboardAvoidingView behaviour='padding' enabled style={styles.container} ref={viewRef}>
             <Text style={styles.titleText}>This is SearchPage</Text>
+            <Button onPress={test} title="Share" />
         </KeyboardAvoidingView>
     );
 }
